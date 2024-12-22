388aa310c3acc1cab36d66c147662f18be72e0b8
Divya <sankarasettydivyasravani@gmail.com>
2024-12-22 17:36:14 +0530

    ain

--- src/components/Projects/ProjectCard.jsx
+++ src/components/Projects/ProjectCard.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 
-const ProjectCard = ({title,main,banner}) => {
+const ProjectCard = ({title,main,banner,Demo,code}) => {
   return (
     <div className='p-3 md:p-6 flex flex-col w-80 bg-[#0c0e19] shadow-xl shadow-slate-900 rounded-2xl'>
         <div className='h-50'>
@@ -12,8 +12,8 @@ const ProjectCard = ({title,main,banner}) => {
         </h3>
         <p className='px-4 text-sm md:text-md leading-tight py-2 mt-4'>{main}</p>
         <div className='mt-2 p-2 md:p-4 flex gap-2 md:gap-4'>
-            <button className='md:mt-10 text-white py-2 px-3 text-sm md:text-lg md:py-2 md:px-4 hover:opacity-85 duration-300 hover:scale-105 font-semibold rounded-3xl bg-[#465697]'>Demo</button>
-            <button className='md:mt-10 text-white py-2 px-3 text-sm md:text-lg md:py-2 md:px-4 hover:opacity-85 duration-300 hover:scale-105 font-semibold rounded-3xl bg-[#465697]'>Source Code</button>
+            <a href={Demo} target="_blank"><button className='md:mt-10 text-white py-2 px-3 text-sm md:text-lg md:py-2 md:px-4 hover:opacity-85 duration-300 hover:scale-105 font-semibold rounded-3xl bg-[#465697]'>Demo</button></a>
+            <a href={code} target="_blank"><button className='md:mt-10 text-white py-2 px-3 text-sm md:text-lg md:py-2 md:px-4 hover:opacity-85 duration-300 hover:scale-105 font-semibold rounded-3xl bg-[#465697]'>Source Code</button></a>
         </div>
     </div>
   )
